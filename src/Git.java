2e1f1fc9bcef3f2dc72a0fbacd90c6a7b8d4672f
cdp055 <davidpiedra224@gmail.com>
2022-10-24 18:17:21 -0500

    claseprogramacion

---
+++ src/Git.java
@@ -0,0 +1,21 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("colocar los comandos git utilizados desde la inicialización del proyecto a  git hasta la colocación de los archivos en github, recuerde colocar una explicación de lo que hace cada comando (de igual forma esta información la presentará con ayuda de la instrucción System.out.println)");
+        System.out.println("git --global user.email davidpiedra224@gmail.com:sirve para poner el correo personal");
+        System.out.println("git config --global user.email cdp055:sirve para ingresar el apodo");
+        System.out.println(": para entrar al proceso del guardado");
+        System.out.println("git init .:guarda el proceso realizado");
+        System.out.println("git add .\.git\ : guardamos en git;");
+        System.out.println("git status:deja ver si los archivos estan bien");
+        System.out.println("git commit -m clasrprogramacion:analiza los cambios realizados en los comandos");
+        System.out.println("git remote add origin:evalua conecciones con otros repositorios");
+        System.out.println("git branch -M main:controla ramas");
+        System.out.println("push -u origin main:para insertar los cambios locales en el repositorio en linea ");
+
+
+
+
+
+    }
+
+}
