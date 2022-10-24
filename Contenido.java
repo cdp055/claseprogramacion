11ea149f34e840d30265a59ee51f3911a7d8fbe3
cdp055 <davidpiedra224@gmail.com>
2022-10-24 16:13:57 -0500

    claseprogramacion

---
+++ Contenido.java
@@ -0,0 +1,26 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("20 conceptos basicos de la programacion");
+
+        System.out.println("Algoritmo: es básicamente un conjunto de pasos para una determinada tarea.");
+        System.out.println("Lenguaje: conjunto de definiciones (sintácticas y semánticas) utilizadas para hacer un texto que la computadora sea capaz de entender, es decir, un programa de computadora.");
+        System.out.println("Código: conjunto de palabras o símbolos que contienen instrucciones para la computadora. ");
+        System.out.println("Código fuente: código de un programa. La computadora lo usa para generar una versión del programa que es capaz de procesar. ");
+        System.out.println("Código Máquina: código que la máquina puede entender y ejecutar.");
+        System.out.println("Código Binario: código formado solo por 0 y 1 que tiene como función codificar algo. Es la base de toda la lógica de la computadora. Todo se convierte, al final, en código binario.");
+        System.out.println("Bug: problema en el código que hace que no ejecute correctamente su función.");
+        System.out.println("Refactorización: proceso de cambiar un programa para mejorar su estructura interna sin cambiar su funcionamiento.");
+        System.out.println("Frameworks: son como colecciones de herramientas, componentes y soluciones que puedes encontrar en un mismo paquete (el framework) que facilitan la creación de aplicaciones complejas.");
+        System.out.println("Queries: solicitud de información y/o datos para una base de datos.");
+        System.out.println("Front-End: es la parte de una aplicación web que la persona que la usa, ve e interactúa. Es ejecutada por el browser.");
+        System.out.println("Back-End: es lo que el usuario no ve en un software, como servidores y base de datos. Es un procesamiento “detrás de escena.");
+        System.out.println("Full-stack: unión de Front-End y Back-End.");
+        System.out.println("SSL (Secure Sockets Layer): es un tipo de seguridad estándar para garantizar que todos los datos estén encriptados en tránsito entre un sitio y un navegador.");
+        System.out.println("Backdoor: puerto creado por alguna aplicación maliciosa para establecer un canal de comunicación desde nuestro dispositivo hasta un punto en el que se puedan manipular nuestros datos o acciones.");
+        System.out.println("Backup: copia de archivos en otro dispositivo para guardar información. ");
+        System.out.println("Bandwidth: ancho de banda. Dice cuánto se puede viajar en una red. ");
+        System.out.println("Benchmarking: herramienta utilizada para medir y comparar el rendimiento de una máquina. ");
+        System.out.println("Big Data: almacenamiento de grandes cantidades de datos.");
+        System.out.println("Bit: abreviatura de binary digit, es decir, representa solo un valor binario (cero o uno).");
+    }
+}
